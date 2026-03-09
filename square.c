7d4c9e43b4086415ca9daeb2ac0b18bf0c5ed299
MOISE N <niyomahoromoise@gmail.com>
2026-03-09 10:37:54 +0200

    Create square.c file to display different square patterns

---
+++ square.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void printSquare(int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the square: ");
+    scanf("%d", &size);
+    printSquare(size);
+    return 0;
+}
